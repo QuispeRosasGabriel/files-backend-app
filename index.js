435ecbca343de432c8d440c1158834728e52bbe1
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-11-29 22:20:28 -0500

    conectando a mongodb atlas

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 // crear servidor
 const app = express();
+
+// conectar la base de datos
+connectDB();
 
 // puerto de la app
 const port = process.env.PORT || 4000;
