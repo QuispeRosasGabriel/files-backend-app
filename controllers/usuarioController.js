2dd693daa7bceaa03ff5edb50c591d3320026c7b
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-01 23:03:34 -0500

    validacion de usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,8 +1,17 @@
 const Usuario = require('../models/Usuario');
 
 exports.nuevoUsuario = async (req,res) => {
-    const usuario = await new Usuario(req.body);
+
+    // verificar si el usuario ya estuvo registrado
+    const {email} = req.body;
+    let usuario = await Usuario.findOne({email});
+
+    if(!!usuario) {
+        return res.status(400).json({msg: 'Error, este usuario ya está registrado'});
+    }
+
+    usuario = await new Usuario(req.body);
     usuario.save();
-    
+
     res.json({msg: 'Usuario creado correctamente'});
 }
