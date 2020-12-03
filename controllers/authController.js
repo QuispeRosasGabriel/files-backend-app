4c0ed6d56e002167c9b5624b8a2dd2687918651b
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-02 22:42:59 -0500

    agregando validacion

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,29 @@
 const Usuario = require('../models/Usuario');
+const bcrypt = require('bcrypt');
 
 exports.autenticarUsuario = async(req,res,next) => {
 
     // Revisar si hay errores
 
     // Verificar el usuario para ver si esta registrado
-    const {email} = req.body
+    const {email, password} = req.body
     const usuario = await Usuario.findOne({email});
     if(!usuario) {
         res.status(401).json({msg: 'El usuario no existe'});
         return next();
     }
-    
+
+    if(bcrypt.compareSync(password, usuario.password)) {
+
+        // Generando JWT
+
+    } else {
+        res.status(401).json({msg: 'Password incorrecto'});
+        return next();
+    }
+
     // Verificar el password y autenticar el usuario
-    console.log(usuario);
+
 
 }
 
