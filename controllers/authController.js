089facda695c8fd63006b4cb3a1cdb6307aa1447
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-02 22:31:44 -0500

    validando usuario

---
+++ controllers/authController.js
@@ -0,0 +1,20 @@
+const Usuario = require('../models/Usuario');
+
+exports.autenticarUsuario = async(req,res,next) => {
+
+    // Revisar si hay errores
+
+    // Verificar el usuario para ver si esta registrado
+    const {email} = req.body
+    const usuario = await Usuario.findOne({email});
+    if(!usuario) {
+        res.status(401).json({msg: 'El usuario no existe'});
+        return next();
+    }
+    
+    // Verificar el password y autenticar el usuario
+    console.log(usuario);
+
+}
+
+exports.usuarioAutenticado = (req,res) => {}
