28e0706389e28a4ad29b996330ae165d8c4d033e
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-05 11:19:32 -0500

    verificando token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,26 @@
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config({path: 'global.env'})
-const {validationResult} = require('express-validator');
+require('dotenv').config({ path: 'global.env' })
+const { validationResult } = require('express-validator');
 
-exports.autenticarUsuario = async(req,res,next) => {
+exports.autenticarUsuario = async (req, res, next) => {
 
     // mostrar mensajes de error de express validator
     const errores = validationResult(req);
-    if(!errores.isEmpty()) {
-         return res.status(400).json ({errores: errores.array()});
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() });
     }
 
     // Verificar el usuario para ver si esta registrado
-    const {email, password} = req.body
-    const usuario = await Usuario.findOne({email});
-    if(!usuario) {
-        res.status(401).json({msg: 'El usuario no existe'});
+    const { email, password } = req.body
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+        res.status(401).json({ msg: 'El usuario no existe' });
         return next();
     }
 
-    if(bcrypt.compareSync(password, usuario.password)) {
+    if (bcrypt.compareSync(password, usuario.password)) {
 
         // Generando JWT
         const token = jwt.sign({
@@ -31,10 +31,10 @@ exports.autenticarUsuario = async(req,res,next) => {
             expiresIn: '8h'
         });
 
-        res.status(200).json({token})
+        res.status(200).json({ token })
 
     } else {
-        res.status(401).json({msg: 'Password incorrecto'});
+        res.status(401).json({ msg: 'Password incorrecto' });
         return next();
     }
 
@@ -43,4 +43,20 @@ exports.autenticarUsuario = async(req,res,next) => {
 
 }
 
-exports.usuarioAutenticado = (req,res) => {}
+exports.usuarioAutenticado = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+
+    if (authHeader) {
+        // Obtener el token        
+        const token = authHeader.split(' ')[1];
+
+        try {
+            // Comprobar el jwt
+            const usuario = jwt.verify(token, process.env.SECRET_KEY);
+            res.json({ usuario });
+        } catch (error) {
+            res.status(401).json({ msg: 'Token no valido', error })
+        }
+    }
+    return next();
+}
