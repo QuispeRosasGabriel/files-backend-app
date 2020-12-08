f3866aadc3e756885d78577b8cd07f87e4ebe2b3
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-07 22:27:13 -0500

    bcrypt a documentos

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,7 +1,8 @@
 const Enlaces = require('../models/Enlace');
 const shortid = require('shortid');
+const bcrypt = require('bcrypt');
 
-exports.nuevoEnlace = async (req,res, next) => {
+exports.nuevoEnlace = async (req, res, next) => {
 
     // revisar si hay errores
 
@@ -13,12 +14,29 @@ exports.nuevoEnlace = async (req,res, next) => {
     enlace.nombre_original = nombre_original;
     enlace.password = password;
 
+    if(req.usuario) {
+        const { password, descargas } = req.body;
+
+        // Asignar a enlace el numero de descargas
+        if(descargas) {
+            enlace.descargas = descargas;
+        }
+
+        // Asignar un password
+        if(password) {
+            const salt = bcrypt.genSalt(10);
+            enlace.password = await bcrypt.hash(password, salt);
+        }
+
+        enlace.autor = req.usuario.id;
+    }
+
     try {
         await enlace.save();
-        return res.status(200).json({msg: `${enlace.url}`});
+        return res.status(200).json({ status: 'Ok', msg: `${enlace.url}` });
         next();
     } catch (error) {
-        
+
     }
 
 }
