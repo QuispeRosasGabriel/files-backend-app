a03079668a853456d2088fa233b6f0fd59cb7a79
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-07 19:46:56 -0500

    guardando doc en mongo

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,7 +1,24 @@
+const Enlaces = require('../models/Enlace');
+const shortid = require('shortid');
+
 exports.nuevoEnlace = async (req,res, next) => {
 
     // revisar si hay errores
 
     // almacenar en la bd
+    const { nombre_original, password } = req.body;
+    const enlace = new Enlaces();
+    enlace.url = shortid.generate();
+    enlace.nombre = shortid.generate();
+    enlace.nombre_original = nombre_original;
+    enlace.password = password;
+
+    try {
+        await enlace.save();
+        return res.status(200).json({msg: `${enlace.url}`});
+        next();
+    } catch (error) {
+        
+    }
 
 }
--- models/Enlace.js
+++ models/Enlace.js
@@ -15,7 +15,8 @@ const enlacesSchema = new Schema({
         required: true,
     },
     descargas: {
-        type: Number
+        type: Number,
+        default: 1,
     },
     autor: {
         type: mongoose.Schema.Types.ObjectId,
@@ -24,6 +25,7 @@ const enlacesSchema = new Schema({
     },
     password: {
         type: String,
+        default: null,
     },
     creado: {
         type: Date,
