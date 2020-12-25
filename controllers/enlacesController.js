3c76c252b37438a45f49e4497a7c5dc235873e99
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-12-25 14:13:51 -0500

    enlaces controller

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -12,10 +12,10 @@ exports.nuevoEnlace = async (req, res, next) => {
     }
 
     // almacenar en la bd
-    const { nombre_original, password } = req.body;
+    const { nombre_original, password, nombre } = req.body;
     const enlace = new Enlaces();
     enlace.url = shortid.generate();
-    enlace.nombre = shortid.generate();
+    enlace.nombre = nombre;
     enlace.nombre_original = nombre_original;
     enlace.password = password;
 
